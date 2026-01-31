3530f552a9daeee061aa1951ba36164ea2414fe2
luisescx <luis.esculapio@live.com>
2026-01-31 12:47:56 -0300

    feat: auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    return request.session.userId;
+  }
+}
