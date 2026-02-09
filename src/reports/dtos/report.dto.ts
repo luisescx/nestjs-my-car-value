14c8efd67598965e9d1ab04e9570b8cda75e11c0
luisescx <luis.esculapio@live.com>
2026-02-09 14:30:58 -0300

    feat: add report and user relations

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,32 @@
+import { Expose, Transform } from 'class-transformer';
+import { Report } from '../report.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lgn: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }: { obj: Report }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
