1f41c5fc685f2d60ea980a8f14ad064aa9378ff7
luisescx <luis.esculapio@live.com>
2026-02-11 14:35:54 -0300

    feat: add current user middleware

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,32 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { UsersService } from '../users.service';
+import { User } from '../user.entity';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: User | null;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private userService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session || {};
+
+    if (userId) {
+      const user = await this.userService.findOne(userId);
+
+      if (user) {
+        req.currentUser = user;
+      }
+    }
+
+    next();
+  }
+}
